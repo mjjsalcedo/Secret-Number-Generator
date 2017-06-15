1c08ce310ea00dfb952d995e60560e6bc1818a58
Mariel Salcedo <Mariel Salcedo>
2017-06-15 13:29:48 -1000

    two-tests-pass

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,13 @@
 'use strict';
 module.exports = function() {
-  
-};
+
+    var generatedNumber = (Math.random()*1000000);
+
+    return function (){
+      var generatedNumber = (Math.random()*1000000);
+    return generatedNumber;
+
+    };
+
+  };
+
