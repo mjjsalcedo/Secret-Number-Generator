19738dbda9377bc798b8ea0d6dc49af15be9a200
Mariel Salcedo <Mariel Salcedo>
2017-06-15 13:39:53 -1000

    let-is-better?

--- secret_number.js
+++ secret_number.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = function() {
 
-    var generatedNumber = (Math.random()*1000000);
+    let generatedNumber = (Math.random()*1000000);
 
     return function (){
       return generatedNumber;
