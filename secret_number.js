d479a7dd94fa2ca79b5d6d2335cfffe6cc8efc78
Mariel Salcedo <Mariel Salcedo>
2017-06-15 14:02:48 -1000

    fixed?

--- secret_number.js
+++ secret_number.js
@@ -1,7 +1,8 @@
+/*jshint esversion: 6 */
 'use strict';
 module.exports = function() {
 
-    let generatedNumber = (Math.random()*1000000);
+    let generatedNumber = (Math.random()*1000001);
 
     return function (){
       return generatedNumber;
